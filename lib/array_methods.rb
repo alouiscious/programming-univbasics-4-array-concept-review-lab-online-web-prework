3bf92c3ffde4891e81df593799302eb05086d16d
Alouiscious Sims <info@reneesoul.com>
2019-07-14 05:11:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  if index = array.index(value_to_find).to_i > 0 ? return index : false
+  index = array.index(value_to_find).to_i > 0 ? return index : false
 
 end
 
