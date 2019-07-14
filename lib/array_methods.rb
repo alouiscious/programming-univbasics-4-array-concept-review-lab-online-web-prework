506ec4004c2bad4688cc682af29eef3357a601e5
Alouiscious Sims <info@reneesoul.com>
2019-07-14 05:09:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  index = array.index(value_to_find).to_i
-  puts index
+  if index = array.index(value_to_find).to_i > 0 ? return index : false
 
 end
 
