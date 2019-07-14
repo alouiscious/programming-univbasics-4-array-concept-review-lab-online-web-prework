5a7ca323699c02d42058a5b313965c1655c3ca67
Alouiscious Sims <info@reneesoul.com>
2019-07-14 04:45:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
   # find_array = [1, 2, 3, 4, 5, 6, 7]
-  find.index(array, 6)
-  return find.index
+  index = find.index(array, 6)
+  return index
 
 end
 
